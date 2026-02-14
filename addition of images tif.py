4c5a784ecb680a3d62c8f9d839adfd8e4268319a
satyajeet <satyajeetpatil1992@gmail.com>
2026-02-14 22:46:30 +0530

    Fourier transform of added images is added

--- addition of images tif.py
+++ addition of images tif.py
@@ -10,7 +10,7 @@ Add = np.zeros((50, 50), dtype=np.float64)
 number_of_files = 1
 
 for ii in range(number_of_files):
-    file_path = f'NF_GaussianEnvelope_SPDC_{ii}.tif'  # Specify your TIFF file path here
+    file_path = f'Masked_NF_GaussianEnvelope_SPDC_{ii}.tif'  # Specify your TIFF file path here
 
     with tiff.TiffFile(file_path) as tif:
         total_frames = min(len(tif.pages), num_frames)
@@ -21,17 +21,24 @@ for ii in range(number_of_files):
             Add += frame1
 
 
-fig, axs = plt.subplots(1, 2, figsize=(18, 5))
+fig, axs = plt.subplots(1, 3, figsize=(18, 5))
 
 # --- Plot 1: RJDP ---
 im0 = axs[0].imshow(Add, origin='lower')
 axs[0].set_title('Nearfield y-axis JDP')
 fig.colorbar(im0, ax=axs[0])
 
-# --- Plot 3: Single frame ---
+# --- Plot 2: Single frame ---
 im2 = axs[1].imshow(frame1, origin='lower')
 axs[1].set_title('Single Frame S=3')
 fig.colorbar(im2, ax=axs[1])
+
+# --- Plot 3: Fourier of summed image ---
+F = np.fft.fftshift(np.fft.fft2(Add))
+im3 = axs[2].imshow(np.log1p(np.abs(F)), origin='lower')
+axs[2].set_title('FFT magnitude (log)')
+fig.colorbar(im3, ax=axs[2])
+
 
 plt.tight_layout()
 plt.show()
